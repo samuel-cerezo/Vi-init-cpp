18c8887836da5a48c41bca77edff9db88daf2eb2
Samuel Cerezo <samuel@gmail.com>
2025-06-17 11:03:07 +0200

    reorganizing files

--- src/main.cpp
+++ src/main.cpp
@@ -13,29 +13,35 @@ int main() {
     std::normal_distribution<double> noise(0.01, 0.005);    // (mean, std)
 
     // Simular omega_all con 3 mediciones de rotación pequeñas
-    std::vector<Eigen::Vector3d> omega_all;
+    std::vector<Eigen::Vector3d> omega_clean;
+    std::vector<Eigen::Vector3d> omega_bias;
     int N=10;   //numero de muestras
-    for(int i=0; i<N ; i++){
-        omega_all.emplace_back(noise(generator), noise(generator), noise(generator));
-    };
     
+    // simular bias real
+    Eigen::Vector3d bias_real(0.07, 0.0, 0.02);
+ 
+    for(int i=0; i<N ; i++){
+        Eigen::Vector3d omega = Eigen::Vector3d(
+        noise(generator), 
+        noise(generator), 
+        noise(generator));
+        omega_clean.push_back(omega);
+        omega_bias.push_back(omega + bias_real);
+    }
 
     double deltat = 0.01; // 10 ms entre mediciones
 
-    // simular bias real
-    Eigen::Vector3d bias_real(0.003, 0.0, 0.0);
-
+ 
     // Preintegrar rotaciones
     Eigen::Matrix3d Rpreint = Eigen::Matrix3d::Identity();
-    for (const auto& omega_cleaned : omega_all) {
-        Eigen::Vector3d omega = omega_cleaned + bias_real;
-        Rpreint *= Eigen::AngleAxisd(omega.norm(), omega.normalized()).toRotationMatrix();
-    }
+    for (const auto& omega : omega_bias) {
+        Rpreint *= lie::ExpMap(omega * deltat);
+     }
 
     // Simulamos Rij visual
     Eigen::Matrix3d Rij = Eigen::Matrix3d::Identity();
-    for(const auto& omega_visual : omega_all){
-        Rij *= Eigen::AngleAxisd(omega_visual.norm(), omega_visual.normalized()).toRotationMatrix();
+    for(const auto& omega : omega_clean){
+        Rij *= lie::ExpMap(omega * deltat);
     }
 
     // Supongamos transformaciones identidad para cuerpo-cámara e IMU
@@ -43,7 +49,7 @@ int main() {
     Eigen::Matrix4d tbodyimu = Eigen::Matrix4d::Identity();
 
     // Calcular el bias con bg_small_angle
-    Eigen::Vector3d bg_est = bg_small_angle(omega_all, deltat, Rpreint, Rij, tbodycam, tbodyimu);
+    Eigen::Vector3d bg_est = bg_small_angle(omega_bias, deltat, Rpreint, Rij, tbodycam, tbodyimu);
 
     std::cout << "Bias giroscópico estimado (bg):\n" << bg_est.transpose() << std::endl;
 
